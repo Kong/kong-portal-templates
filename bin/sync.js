a00ca230c2a34440ee46725dc1bc5a4a98dce2c6
Nathan Bailey <nbailey0001@gmail.com>
2019-04-16 15:45:08 -0700

    feat(sync) do not attempt to delete files on read (#25)

--- bin/sync.js
+++ bin/sync.js
@@ -176,8 +176,6 @@ async function read (directory, type) {
 
     // Delete remote file if it exists, recreate it from local file
     return Promise.resolve()
-      .then(() => handle(type, filename, path, getFile))
-      .then(res => deleteExistingFile(res, type, path))
       .then(() => handle(type, filename, path, createFile))
       .then(res => handleResponse(res, 'create', isCreated, type, path))
       .catch(err => console.log(err))
@@ -453,7 +451,7 @@ async function init () {
 
   if (PUSH) {
     let proceed = NO_PROMPT || await promptForPermission(`\n!!!WARNING!!!\n\nYou are about to push all files located in ${DIRECTORY} to ${apiURL}?\nThis will replace remote files that share the same name and cannot be undone!\n\nProceed? (y/n).\n`)
-    
+
     if (proceed) {
       console.log(`pushing files to: ${apiURL}`)
       await read(DIRECTORY)
