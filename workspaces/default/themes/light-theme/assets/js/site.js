d18746ff36bc8695adba0da14b13d9a408c700ff
nijikokun <nijikokun@gmail.com>
2019-07-22 14:26:49 -0700

    feat(*): palette updates, hero image, auth updates

--- workspaces/default/themes/light-theme/assets/js/site.js
+++ workspaces/default/themes/light-theme/assets/js/site.js
@@ -1,6 +1,6 @@
 // Taken from underscore.js | MIT
 // Used for input keydown events
-var debounce = function (func, wait, immediate) {
+var debounce = function(func, wait, immediate) {
 	var timeout;
 	return function() {
 		var context = this, args = arguments;
@@ -14,3 +14,16 @@ var debounce = function (func, wait, immediate) {
 		if (callNow) func.apply(context, args);
 	};
 };
+
+// Taken from StackOverflow | MIT
+// Used for QueryString parsing (ES3+ supported)
+// See login/register forms for usage
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
