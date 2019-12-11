49481109ef1f2df3e20f4194438315acc772aa22
Jordan Lawler <jordanalawler@gmail.com>
2019-12-10 17:07:33 -0800

    feat(spec-renderer): add additional render options for spec-renderer (#107)

--- workspaces/default/themes/base/assets/js/kong/kong.js
+++ workspaces/default/themes/base/assets/js/kong/kong.js
@@ -1,1 +1,35 @@
-window.Kong = {}
+window.Kong = window.Kong || {}
+window.Kong.SpecRenderer = window.Kong.SpecRenderer || {}
+
+window.Kong.SpecRenderer.getSpec = function(url) {
+  return new Promise((res, rej) => {
+    $.ajax({
+      type: 'GET',
+      dataType: 'text',
+      url: url,
+      crossDomain: true,
+      xhrFields: {
+        withCredentials: true
+      },
+
+      success: function(data) {
+        return res(data);
+      },
+
+      error: function(xhr, ajaxOptions, throwError) {
+        if (xhr.responseText && xhr.responseText != "OK") {
+          try {
+            xhr.responseJSON = JSON.parse(xhr.responseText)
+          } catch (e) {}
+        }
+
+        rej({
+          message: xhr.responseJSON ? xhr.responseJSON.message : null,
+          xhr: xhr,
+          ajaxOptions: ajaxOptions,
+          throwError: throwError
+        });
+      }
+    });
+  })
+}
